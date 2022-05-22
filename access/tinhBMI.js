51d785e86ebde251b1bf8b3cff19b1a693e038be
lebinhthuan <104605059+lebinhthuan@users.noreply.github.com>
2022-05-23 01:06:13 +0700

    first commit

---
+++ access/tinhBMI.js
@@ -0,0 +1,16 @@
+let canNang=+prompt('moi ban nhap vao can nang')
+
+
+
+
+
+if (bmi < 18)
+    document.write("Gầy đấy! Ăn nhiều chút!");
+else if (bmi < 25.0)
+    document.write("Bình thường");
+else if (bmi < 30.0)
+    document.write("Hơi mập rồi!");
+else if (bmi >= 30.0)
+    document.write("Quá mập! Giảm cân thôi!");
+else
+    document.write('Không có số liệu tính toán, tôi tôi bó tay!')
